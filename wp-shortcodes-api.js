aed9faef18fec634029f67b0aacbb6e56ea9d662
Mark Parolisi <mark@voceconnect.com>
2012-01-11 20:34:56 -0500

    ensuring that the regex calls have a match before prepopulating the popup form

--- wp-shortcodes-api.js
+++ wp-shortcodes-api.js
@@ -1,86 +1,92 @@
 jQuery(document).ready(function($){
     
+    $('#wp-shortcode input[type="text"]').live('keyup', function(){
+        var preview = build_shortcode();
+        $('#shortcode-preview').text(preview);
+    })
 
-	var win = window.dialogArguments || opener || parent || top;
-	var selection = win.tinymce.activeEditor.selection.getContent( {
-		'format' : 'text'
-	} );
+    var win = window.dialogArguments || opener || parent || top;
+    var selection = win.tinymce.activeEditor.selection.getContent( {
+        'format' : 'text'
+    } );
 
-	if(selection){
-		var sc = selection.match(/[^\s\[]([^ ]*)[^\] ]/gi);
+    if(selection){
+        var sc = selection.match(/[^\s\[]([^ ]*)[^\] ]/gi);
 
-		if(sc && sc.length){
-			set_field_val();
-			$('#wp-shortcode input[type="text"]').trigger('keyup');
-		}
-	}
+        if(sc && sc.length){
+            set_field_val();
+            $('#wp-shortcode input[type="text"]').keyup();
+        }
+    }
 
-	function set_field_val(){
-		var shortcode_name = sc[0];
-		var current_attr = '';
-		var current_attr_name = '';
-		var current_attr_value = '';
+    function set_field_val(){
+        var shortcode_name = sc[0];
+        var current_attr = '';
+        var current_attr_name = '';
+        var current_attr_value = '';
 
-		var selected_shortcode = $('#shortcode-name').val();
+        var selected_shortcode = $('#shortcode-name').val();
 
-		if (shortcode_name == selected_shortcode) {
+        if (shortcode_name == selected_shortcode) {
 
-			var sc_attrs = selection.match(/([^\s]*=".*?")/gi);
+            var sc_attrs = selection.match(/([^\s]*=".*?")/gi);
+            if(sc_attrs.length){
+                for(var index in sc_attrs) {
+                    current_attr = sc_attrs[index].split('=');
+                    current_attr_name = current_attr[0];
+                    if(current_attr[1] != null){
+                        current_attr_value = current_attr[1].match(/[^"].*[^"]/gi);
+                        if(current_attr_value != null){
+                            current_attr_value = current_attr_value[0]
+                        }
+                    }
+                    if(current_attr_value != null){
+                        $('#'+current_attr_name).val(current_attr_value);
+                    }
+                }
+            }
+        }
 
-			for(var index in sc_attrs) {
-				current_attr = sc_attrs[index].split('=');
+        return true;
+    }
 
-				current_attr_name = current_attr[0];
-				current_attr_value = current_attr[1].match(/[^"].*[^"]/gi)[0];
-
-				$('#'+current_attr_name).val(current_attr_value);
-			}
-		}
-
-		return true;
-	}
-
-	function get_shortcode_atts(){
-		var formValsString = "";
-		// loop through the input fields and create our attributes string
-		$('#wp-shortcode input[type="text"]').each(function(){
-			if($(this).val().length){
-				formValsString += " " + $(this).attr('name').trim() + "=\""+ $(this).val().trim()+"\"";
-			}
-		})
-		return formValsString;
-	}
+    function get_shortcode_atts(){
+        var formValsString = "";
+        // loop through the input fields and create our attributes string
+        $('#wp-shortcode input[type="text"]').each(function(){
+            if($(this).val().length){
+                formValsString += " " + $(this).attr('name').trim() + "=\""+ $(this).val().trim()+"\"";
+            }
+        })
+        return formValsString;
+    }
     
-	function build_shortcode(){
-		var shortcodeName = $('#shortcode-name').val();
-		var formValsString = get_shortcode_atts();
-		return "["+ shortcodeName + formValsString +"]";
-	}
+    function build_shortcode(){
+        var shortcodeName = $('#shortcode-name').val();
+        var formValsString = get_shortcode_atts();
+        return "["+ shortcodeName + formValsString +"]";
+    }
     
     
-	$('#submit-shortcode-api').live('click', function(e){
+    $('#submit-shortcode-api').live('click', function(e){
 
-		var shortcodeString = build_shortcode();
+        var shortcodeString = build_shortcode();
         
-		// insert into the editor
-		var win = window.dialogArguments || opener || parent || top;
+        // insert into the editor
+        var win = window.dialogArguments || opener || parent || top;
 
-		var isVisual = (typeof win.tinyMCE != "undefined") && win.tinyMCE.activeEditor && !win.tinyMCE.activeEditor.isHidden();
-		if (isVisual) {
-			win.tinyMCE.activeEditor.execCommand('mceInsertContent', false, shortcodeString);
-		} else {
-			var currentContent = jQuery('#content', window.parent.document).val();
-			if ( typeof currentContent == 'undefined' )
-				currentContent = '';
-			jQuery( '#content', window.parent.document ).val( currentContent + shortcodeString );
-		}
+        var isVisual = (typeof win.tinyMCE != "undefined") && win.tinyMCE.activeEditor && !win.tinyMCE.activeEditor.isHidden();
+        if (isVisual) {
+            win.tinyMCE.activeEditor.execCommand('mceInsertContent', false, shortcodeString);
+        } else {
+            var currentContent = jQuery('#content', window.parent.document).val();
+            if ( typeof currentContent == 'undefined' )
+                currentContent = '';
+            jQuery( '#content', window.parent.document ).val( currentContent + shortcodeString );
+        }
         
-		// close the thickbox
-		self.parent.tb_remove();
-	})
+        // close the thickbox
+        self.parent.tb_remove();
+    })
     
-	$('#wp-shortcode input[type="text"]').live('keyup', function(){
-		var preview = build_shortcode();
-		$('#shortcode-preview').text(preview);
-	})
 })
